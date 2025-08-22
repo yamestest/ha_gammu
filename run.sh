1db076992dcd298b6a998626b4faac0698080ff5
yamestest <yamestest@gmail.com>
2025-08-22 13:01:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/with-contenv bashio
+
+# Získať device z konfigurácie add-onu
 DEVICE=$(bashio::config 'device')
 
 echo "Používam modem na $DEVICE"
-/usr/bin/gammu --device "$DEVICE" --identify
+
+# Vytvoriť konfiguračný súbor pre Gammu, ak ešte neexistuje
+CONFIG_FILE="/data/gammurc"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Vytváram konfiguračný súbor $CONFIG_FILE"
+    cat <<EOF > "$CONFIG_FILE"
+[gammu]
+device = $DEVICE
+connection = at115200
+EOF
+fi
+
+# Test pripojenia modemu
+echo "Testujem modem..."
+gammu -c "$CONFIG_FILE" --identify
+
+if [ $? -eq 0 ]; then
+    echo "Modem je pripravený ✅"
+else
+    echo "Chyba: modem sa nepodarilo identifikovať ❌"
+fi
 
 # Tu môžeš spustiť službu, ktorá počúva na SMS
+# Príklad:
+# gammu-smsd -c /data/gammu-smsdrc
