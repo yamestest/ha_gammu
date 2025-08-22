37dc987b3ecd8ebf9d49f1bb2499b7aa80f8bcb3
yamestest <yamestest@gmail.com>
2025-08-22 13:52:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
+# Získaj device zo setupu HA
 DEVICE=$(bashio::config 'device')
 CONFIG_FILE="/data/gammurc"
 
@@ -9,12 +10,26 @@ cat <<EOF > "$CONFIG_FILE"
 [gammu]
 device = $DEVICE
 connection = at115200
+
+[smsd]
+service = files
+logfile = /data/gammu-smsd.log
+inboxpath = /data/inbox/
+outboxpath = /data/outbox/
+sentpath = /data/sent/
+errorspath = /data/errors/
 EOF
+
+# Vytvor priečinky pre SMS
+mkdir -p /data/inbox /data/outbox /data/sent /data/errors
 
 echo "Používam modem na $DEVICE"
 
-# Otestuj modem, ale container nespadne pri chybe
+# Otestuj modem
 gammu -c "$CONFIG_FILE" --identify || echo "Chyba: nepodarilo sa identifikovať modem"
 
-# Udrží container bežať snad
+# Spusti SMS daemon na pozadí
+gammu-smsd -c "$CONFIG_FILE" &
+
+# Container drží bežať
 tail -f /dev/null
