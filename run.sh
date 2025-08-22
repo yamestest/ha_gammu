69d588a79617f9d23638a312626d1cfb725d5aae
yamestest <yamestest@gmail.com>
2025-08-22 13:48:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,5 +16,5 @@ echo "Používam modem na $DEVICE"
 # Otestuj modem, ale container nespadne pri chybe
 gammu -c "$CONFIG_FILE" --identify || echo "Chyba: nepodarilo sa identifikovať modem"
 
-# Udrží container bežať
+# Udrží container bežať snad
 tail -f /dev/null
